edf118a763269a03096e975b00166e8e620b467b
Graf-Lunin <151855578+Graf-Lunin@users.noreply.github.com>
2025-09-10 10:31:17 +0300

    Create flasr_server.py

---
+++ flasr_server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import asyncio
+import os
+import aiohttp
+import threading
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Пустой сервер работает!!"
+
+async def ping_site():
+    url = os.getenv('URL_FOR_PING')
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                print(f'Pinged site, response status: {response.status}')
+    except Exception as e:
+        print(f'Error pinging site: {e}')
+
+def run_ping_site():
+    asyncio.run(ping_site())
+
+def flask_run():
+    print("Запуск Flask-сервера...")
+    port = int(os.getenv('PORT'))
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=port)
+
+def start_flask_serve():
+    flask_threadd = threading.Thread(target=flask_run)
+    flask_threadd.start()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(run_ping_site, 'interval', minutes=10)
+
+    scheduler.start()
